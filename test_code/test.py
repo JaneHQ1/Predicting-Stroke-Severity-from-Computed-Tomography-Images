5327c2ead146d5b2e5fac7fd2a0e29d16f0ee2ae
Scarlett <janejiang@hotmail.com>
2019-03-07 16:49:27 +1100

    Update test.py

--- test_code/test.py
+++ test_code/test.py
@@ -1,45 +1,17 @@
-''''
-def demo():
-    c=10
-    for i in range(0,9):
-       c+=1;
-    print(c)
-    ''print 的位置很关键'
-demo()
-
-'''''
-
-
-''''
-def demo():
-
-    #global c
-    'global在不在有区别哦'
-    c = 10
-demo()
-print(c)
-'''
-
-# class Student():
-#     name = 'Jin'
-#     age = 26
-#     def print_file(self):
-#         print('name:'+self.name)
-#         print('age:'+str(self.age))
-#student = Student()
-#student.print_file()
+"""
+    Use matplotlib to plot x and y plane
+"""
 
 import pydicom
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-#from glob3 import *
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import scipy.ndimage
 from skimage import morphology
 from skimage import measure
 from skimage.transform import resize
-from sklearn.cluster import KMeans
+# from sklearn.cluster import KMeans
 from plotly import __version__
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 from plotly.tools import FigureFactory as FF
@@ -60,48 +32,37 @@ def load_scan(path):
     return slices
 
 
-def voxel3d(dcms,j):
+def voxel_3d_pixel(dcms,j):
+    voxel_list = []
     row_vector = dcms[j].ImageOrientationPatient[0:3]
     column_vector = dcms[j].ImageOrientationPatient[3:6]
     pixel_size = list(map(float, list(dcms[j].PixelSpacing)))
+
 
     pixel_size_x = pixel_size[0]
     pixel_size_y = pixel_size[1]
     image_position_patient = [float(i) for i in list(dcms[j].ImagePositionPatient)]
 
-
-
-    for m in range(dcms[j].Columns):
+    for m in range(dcms[j].Rows):
         pixel_location_x = m
-        row_change_x = [i * pixel_size_x * float(pixel_location_x) for i in row_vector ]
+        row_change_x = [i * pixel_size_x * float(pixel_location_x) for i in row_vector]
+        # print(row_change_x)
 
-        for n in range(dcms[j].Rows):
+        for n in range(dcms[j].Columns):
             pixel_location_y = n
-            row_change_y = [i * pixel_size_y * float(pixel_location_y) for i in row_vector ]
+            row_change_y = [i * pixel_size_y * float(pixel_location_y) for i in column_vector]
+            # print(row_change_y)
+            voxel = list(map(lambda x: x[0] + x[1] + x[2], list(zip(image_position_patient, row_change_x, row_change_y))))
+            voxel_list.append(voxel)
 
-            voxel = list(map(lambda x: x[0] + x[1] + x[2], zip(image_position_patient, row_change_x, row_change_y)))
-            voxel = np.array(voxel)
-            # voxel_list = np.stack(voxel_list, np.array(voxel))
-
-            return voxel
-
-
-
-
-    # np.stack(arrays, axis=0).shape
-
-
+    return voxel_list
 
 
 id = 0
-data_path = r"C:\Users\Jin\Downloads\Predicting-Stroke-Severity-from-Computed-Tomography-Images-master\Test_Image"
+data_path = r"C:\Users\janej\OneDrive\MelbUni\MASTER OF ENGINEERING\CapstoneProject_2018\Test_Images\test_images10"
 patient = load_scan(data_path)
-
-voxel = voxel3d(patient,0)
-voxel_list = np.array([])
-voxel_list = np.stack(voxel_list, voxel)
+voxel_list = voxel_3d_pixel(patient, 0)
 print(voxel_list)
-
-a=1
-
+# t = np.array(voxel_list).shape
+# print(t)
 
