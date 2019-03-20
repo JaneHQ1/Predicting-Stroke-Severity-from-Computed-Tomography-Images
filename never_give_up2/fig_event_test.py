0ec79ccb9d3f4160c48ed819ccc91f2839f34a23
Scarlett <janejiang@hotmail.com>
2019-03-20 16:15:29 +1100

    Add files via upload

---
+++ never_give_up2/fig_event_test.py
@@ -0,0 +1,102 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class ImUpdate(object):
+    def __init__(self, vol_tran, vol_fron, vol_sagi, ax_tran, ax_fron, ax_sagi):
+        self.vol_tran = vol_tran
+        self.vol_fron = vol_fron
+        self.vol_sagi = vol_sagi
+
+        self.ax_tran = ax_tran
+        self.ax_fron = ax_fron
+        self.ax_sagi = ax_sagi
+
+        self.ax_tran.index = vol_tran.shape[0] // 2
+        self.ax_fron.index = vol_fron.shape[0] // 2
+        self.ax_sagi.index = vol_sagi.shape[0] // 2
+
+        self.lx_tran = ax_tran.axhline(color='b', linewidth=0.8)
+        self.ly_tran = ax_tran.axvline(color='b', linewidth=0.8)
+        self.lx_fron = ax_fron.axhline(color='b', linewidth=0.8)
+        self.ly_fron = ax_fron.axvline(color='b', linewidth=0.8)
+
+        self.txt_tran = ax_tran.text(0.7, 0.9, '', color='b')
+        self.txt_fron = ax_fron.text(0.7, 0.9, '', color='b')
+
+    def control_events(self, event):
+        fig = event.canvas.figure
+        ax_tran = fig.axes[0]
+        ax_fron = fig.axes[1]
+        ax_sagi = fig.axes[2]
+        x_index = np.int_(event.xdata)
+        y_index = np.int_(event.ydata)
+
+        if ax_tran.contains(event)[0]:
+            plt.connect('button_press_event', self.add_cursor(event, self.ax_tran))
+            fig.canvas.mpl_connect('button_press_event', self.frontal_view(event, x_index))
+            fig.canvas.mpl_connect('button_press_event', self.sagittal_view(event, y_index))
+
+        elif ax_fron.contains(event)[0]:
+            fig.canvas.mpl_connect('button_press_event', self.transverse_view(event, y_index))
+            fig.canvas.mpl_connect('button_press_event', self.sagittal_view(event, x_index))
+
+        elif ax_sagi.contains(event)[0]:
+            fig.canvas.mpl_connect('button_press_event', self.transverse_view(event, y_index))
+            fig.canvas.mpl_connect('button_press_event', self.frontal_view(event, x_index))
+
+    def transverse_view(self, event, index):
+        fig = event.canvas.figure
+        ax_tran = fig.axes[0]
+        ax_tran.index = index
+        ax_tran.images[0].set_array(self.vol_tran[ax_tran.index])
+        fig.canvas.draw()
+
+    def frontal_view(self, event, index):
+        fig = event.canvas.figure
+        ax_fron = fig.axes[1]
+        ax_fron.index = index
+        ax_fron.images[0].set_array(self.vol_fron[ax_fron.index])
+        fig.canvas.draw()
+
+
+
+    def sagittal_view(self, event, index):
+        fig = event.canvas.figure
+        ax_sagi = fig.axes[2]
+        ax_sagi.index = index
+        ax_sagi.images[0].set_array(self.vol_sagi[ax_sagi.index])
+        fig.canvas.draw()
+
+
+    def add_cursor(self, event, ax):
+        if ax is self.ax_tran:
+            x, y, z = event.xdata, event.ydata, self.ax_tran.index
+            coord = [x, y, z]
+            self.plot_cursor(coord)
+
+
+    def plot_cursor(self, coord):
+        x, y, z = coord[0], coord[1], coord[2]
+        self.lx_tran.set_ydata(y)
+        self.ly_tran.set_xdata(x)
+
+        self.txt_tran.set_text('x=%1d y=%1d z=%1d' % (x, y, z))
+        plt.draw()
+
+
+
+
+
+
+    # def mouse_cursor(self, event):
+    #     if not event.inaxes:
+    #         return
+    #
+    #     x, y = event.xdata, event.ydata
+    #     z = self.ax_tran.index
+    #     # update the line positions
+    #     self.lx_tran.set_ydata(y)
+    #     self.ly_tran.set_xdata(x)
+    #
+    #     self.txt_tran.set_text('x=%1d y=%1d z=%1d' % (x, y, z))
+    #     plt.draw()
---
+++ never_give_up2/plot_main.py
@@ -0,0 +1,41 @@
+'''
+
+'''
+import nibabel as nib
+# import matplotlib.transforms
+from fig_event_test import *
+
+
+path = r"C:\Users\janej\OneDrive\MelbUni\MASTER OF ENGINEERING\CapstoneProject_2018\Test_Images\TestSample\nifti_2\ARTERIELLE.nii.gz"
+struct = nib.load(path)
+struct_arr = struct.get_fdata()
+# multi_plane_viewer(struct_arr)
+
+vol_tran = struct_arr.transpose(2, 1, 0)
+vol_fron = struct_arr.transpose(1, 2, 0)
+vol_sagi = struct_arr.transpose(0, 2, 1)
+
+fig = plt.figure()
+ax_tran = fig.add_subplot(221)
+# plt.axis('off')
+ax_tran.index = vol_tran.shape[0] // 2
+ax_tran.imshow(vol_tran[ax_tran.index], cmap="gray")
+
+ax_fron = fig.add_subplot(223)
+# plt.axis('off')
+ax_fron.index = vol_fron.shape[0] // 2
+ax_fron.imshow(vol_fron[ax_fron.index], cmap="gray")
+
+ax_sagi = fig.add_subplot(224)
+# plt.axis('off')
+ax_sagi.index = vol_sagi.shape[0] // 2
+ax_sagi.imshow(vol_sagi[ax_sagi.index], cmap="gray")
+
+im_update = ImUpdate(vol_tran, vol_fron, vol_sagi, ax_tran, ax_fron, ax_sagi)
+fig.canvas.mpl_connect('button_press_event', im_update.control_events)
+
+# cursor = Cursor(ax_tran, ax_fron, ax_sagi, vol_tran, vol_fron, vol_sagi)
+# plt.connect('button_press_event', Cursor.check)
+
+
+plt.show()
