5a94177c5be71f1de9b667a2c4e8b461649a3051
ntenzin <48191669+ntenzin@users.noreply.github.com>
2019-04-09 11:42:48 +1000

    Create roi_controller_0409.py

---
+++ Beautiful_code/roi_controller_0409.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  4 12:48:21 2019
+Title: MP4-Medical Image Processing
+@author: MP4 Team
+
+"""
+# ROI Controller for scrolling image
+class RoiController():
+    def __init__(self, fig, im_dis, vol_tran, vol_fron, vol_sagi, ax_tran, ax_fron, ax_sagi):
+        self.fig = fig
+        self.im_dis = im_dis
+
+        self.vol_tran = vol_tran
+        self.vol_fron = vol_fron
+        self.vol_sagi = vol_sagi
+
+        self.ax_tran = ax_tran
+        self.ax_fron = ax_fron
+        self.ax_sagi = ax_sagi
+
+        self.scroll_tran = None
+        self.scroll_fron = None
+        self.scroll_sagi = None
+
+        self.fig.canvas.mpl_connect('axes_enter_event', self.fig_enter_event)
+        self.fig.canvas.mpl_connect('axes_leave_event', self.fig_leave_event)
+
+    def fig_enter_event(self, event):
+        if self.ax_tran.in_axes(event):
+            self.scroll_tran = self.fig.canvas.mpl_connect('scroll_event', self.transverse_scroll)
+
+        elif self.ax_fron.in_axes(event):
+            self.scroll_fron = self.fig.canvas.mpl_connect('scroll_event', self.frontal_scroll)
+
+        elif self.ax_sagi.in_axes(event):
+            self.scroll_sagi = self.fig.canvas.mpl_connect('scroll_event', self.sagittal_scroll)
+
+    def fig_leave_event(self, event):
+        self.fig.canvas.mpl_disconnect(self.scroll_tran)
+        self.fig.canvas.mpl_disconnect(self.scroll_tran)
+        self.fig.canvas.mpl_disconnect(self.scroll_sagi)
+
+    def transverse_scroll(self, event):
+        if event.button == 'down':
+            self.ax_tran.index -= 1
+        if event.button == 'up':
+            self.ax_tran.index += 1
+        self.im_dis.update_transverse_display(self.ax_tran.index)
+        self.fig.canvas.draw_idle()
+
+    def frontal_scroll(self, event):
+        if event.button == 'down':
+            self.ax_fron.index -= 1
+        if event.button == 'up':
+            self.ax_fron.index += 1
+        self.im_dis.update_frontal_display(self.ax_fron.index)
+        self.fig.canvas.draw_idle()
+
+    def sagittal_scroll(self, event):
+        if event.button == 'down':
+            self.ax_sagi.index -= 1
+        if event.button == 'up':
+            self.ax_sagi.index += 1
+        self.im_dis.update_sagittal_display(self.ax_sagi.index)
+        self.fig.canvas.draw_idle()
+        
